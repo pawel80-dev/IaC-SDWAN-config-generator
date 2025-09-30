16753805caa225d5d3ecee28a67db6839ce14678
Pawel <pawel80@gmail.com>
2025-09-30 18:52:17 +0200

    fa_py_azure_v12

--- source/function_app.py
+++ source/function_app.py
@@ -17,7 +17,7 @@ app = func.FunctionApp()
 
 
 # route parameter is changed: api/{functionname} to api/req
-# https://functionAppName.azurewebsites.net/api/req?user=YourName
+# https://functionAppName.azurewebsites.net/api/get?user=YourName
 @app.function_name(name="HttpTrigger-basic")
 @app.route(route="get", auth_level=func.AuthLevel.ANONYMOUS)
 def get_basic(req: func.HttpRequest) -> str:
@@ -26,10 +26,18 @@ def get_basic(req: func.HttpRequest) -> str:
     return f"Hello, {user}!"
 
 
+# https://functionAppName.azurewebsites.net/api/cfg?site_id=YourSiteID
 @app.function_name(name="ConfigGenerator")
 @app.route(route="cfg", auth_level=func.AuthLevel.ANONYMOUS)
 def conf_gen(req: func.HttpRequest) -> str:
     logging.info("AZ-FUNC ConfigGenerator started.")
+    MANAGER_URL = os.environ["MANAGER_URL"]
+    MANAGER_LOGIN = os.environ["MANAGER_LOGIN"]
+    MANAGER_PASS = os.environ["MANAGER_PASS"]
+    SMTP_SERVER = os.environ["SMTP_SERVER"]
+    EMAIL_SENDER = os.environ["EMAIL_SENDER"]
+    EMAIL_SENDER_PASSWORD = os.environ["EMAIL_SENDER_PASSWORD"]
+    EMAIL_RECEIVER = os.environ["EMAIL_RECEIVER"]
     site_id = req.params.get("site_id")
     attachment_name = "ciscosdwan.cfg"
     email_subject = f"Bootstrap config for {site_id}"
