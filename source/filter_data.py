248c98e25d54ac1c370913dca2c31fb479b48c75
Pawel <pawel80@gmail.com>
2025-10-01 20:25:03 +0200

    fa_py_v51

--- source/filter_data.py
+++ source/filter_data.py
@@ -4,6 +4,16 @@ import logging
 # Logging on the INFO level
 logging.basicConfig(level=logging.INFO)
 
+# SDWAN inteface - tunnel mapping
+# https://lostintransit.se/2022/05/14/using-python-to-calculate-cisco-sd-wan-tunnel-numbers-part-1/?doing_wp_cron=1758563977.3016180992126464843750
+# Loopback0                 - Tunnel14095000
+# GigabitEthernet0/0/0      - Tunnel0
+# GigabitEthernet0/0/1      - Tunnel1
+# GigabitEthernet0/0/2      - Tunnel2
+# GigabitEthernet0/1/0      - Tunnel10
+# GigabitEthernet1/0/0      - Tunnel100
+# TengigabitEthernet0/1/0   - Tunnel10000010
+
 
 GH_PAT_TOKEN = ""
 GH_OWNER = ""
