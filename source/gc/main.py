34dd7506403a3cf3a7f0d1110ce1a5417c15a02a
Pawel <pawel80@gmail.com>
2025-10-16 20:54:30 +0200

    gc_func_v33

--- source/gc/main.py
+++ source/gc/main.py
@@ -9,24 +9,12 @@ ipify_url = "https://api.ipify.org"
 # HTTP handler function
 @functions_framework.http
 def hello_world(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    Note:
-        For more information on how Flask integrates with Cloud
-        Functions, see the `Writing HTTP functions` page.
-        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
-    """
-    if request.args.get("check"):
+    path = request.path
+    if path == '/check':
         return "Check Done!"
-    # ip = request.args.get("check")
-    # if ip == "ip":
-    #     public_ip = get_public_ip(ipify_url)
-    #     return f"Your public IP is {public_ip}."
+    elif path == '/ip':
+        public_ip = get_public_ip(ipify_url)
+        return f"Your public IP is {public_ip}."
     else:
         return "Hello World!"
+
