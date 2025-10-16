2575edb8b9fb45290878b54d3191d4ca66b8c203
Pawel <pawel80@gmail.com>
2025-10-16 21:06:34 +0200

    gc_func_v34

--- source/gc/main.py
+++ source/gc/main.py
@@ -6,15 +6,16 @@ from ipify import get_public_ip
 ipify_url = "https://api.ipify.org"
 
 
-# HTTP handler function
+# HTTP handler function (Flask based)
+# https://tedboy.github.io/flask/generated/generated/flask.Request.html
 @functions_framework.http
 def hello_world(request):
     path = request.path
-    if path == '/check':
-        return "Check Done!"
-    elif path == '/ip':
+    user = request.args.get("user")
+    if path == "/ipcheck":
         public_ip = get_public_ip(ipify_url)
         return f"Your public IP is {public_ip}."
+    if path == "/get" and user:
+        return f"Hello, {user}!"
     else:
         return "Hello World!"
-
